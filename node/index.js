284ff6ec9fd04ff35dc9f99ddee0cfa64e01f701
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-04-28 21:06:20 -0300

    refactor: genrate fake name & render view

--- node/index.js
+++ node/index.js
@@ -1,12 +1,20 @@
-const { response } = require('express')
-const express = require('express')
-const app = express()
+const { response } = require('express');
+var fs = require('fs');
+var faker = require('faker');
+const express = require('express');
+const app = express();
 
+var myCss = {
+    style: fs.readFileSync('./views/style.css', 'utf8')
+};
+
+app.set('view engine', 'ejs');
 app.get('/', async(req, res) => {
+    var randomName = faker.name.findName();
     var { connection } = await require("./db");
-    const [rows] = await connection.execute('SELECT * FROM `people`');
-    res.send({ rows });
-
+    await connection.execute(`INSERT INTO people (name) VALUES ('${randomName}')`);
+    const [people] = await connection.execute('SELECT * FROM `people`');
+    res.render('index', { people, randomName, myCss });
 })
 
 app.listen(3000, () => {
